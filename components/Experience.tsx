a36f8c4af2ec18058eecee41d70c50e4b332aee5
Robbie Byrd <me@robbiebyrd.com>
2025-03-30 18:05:23 -0500

    Updates

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import {components} from "../api/strapi"
+import React from 'react'
+import { components } from '../api/strapi'
 import { Skill } from '@/components/Skill'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
 interface ExperienceProps {
-    experience: components["schemas"]["Experience"];
+    experience: components['schemas']['Experience'];
 }
 
 export const Experience: React.FC<ExperienceProps> = ({ experience }) => {
-    const skills = experience.skills?.sort((a, b) => (a.skill_type?.sort ?? 0) - (b.skill_type?.sort ?? 0));
 
-    const startDate = new Date(Date.parse(experience.start || "0")).toLocaleDateString('en-us', { year:"numeric", month:"long"})
-    const endDate = experience.end ? new Date(Date.parse(experience.end || "0")).toLocaleDateString('en-us', { year:"numeric", month:"long"}) : undefined
+    const dateFormat: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long' }
+    const startDate = new Date(Date.parse(experience.start || '0')).toLocaleDateString('en-us', dateFormat)
+    const endDate = experience.end ? new Date(Date.parse(experience.end || '0')).toLocaleDateString('en-us', dateFormat) : undefined
+
+    const sortedSkills = experience.skills?.sort((a, b) => (a.skill_type?.sort ?? 0) - (b.skill_type?.sort ?? 0))
 
     return (
         <div>
@@ -22,14 +24,14 @@ export const Experience: React.FC<ExperienceProps> = ({ experience }) => {
             <h2>
                 <ReactMarkdown remarkPlugins={[remarkGfm]}>{experience.subtitle}</ReactMarkdown>
             </h2>
-            <p>{startDate} - {endDate || "Current"}</p>
+            <p>{startDate} - {endDate || 'Current'}</p>
             <ReactMarkdown remarkPlugins={[remarkGfm]}>{experience.summary}</ReactMarkdown>
             <ReactMarkdown remarkPlugins={[remarkGfm]}>{experience.description}</ReactMarkdown>
-            <div className={"w-full flex flex-wrap"}>
-                {skills?.map((skill, index) => (
+            <div className={'w-full flex flex-wrap'}>
+                {sortedSkills?.map((skill, index) => (
                     <Skill key={index} skill={skill} size={'medium'} />
                 ))}
             </div>
         </div>
-    );
-};
+    )
+}
